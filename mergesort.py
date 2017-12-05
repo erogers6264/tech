69e5b18baaec7825b80a9f2d6f7b1d056fcd4ba7
Ethan Rogers <erogers6264@gmail.com>
2017-12-04 19:16:58 -0700

    Create skeleton for merge sort

---
+++ mergesort.py
@@ -0,0 +1,15 @@
+# mergesort.py, Ethan Rogers
+# Description: A program that sorts an array
+# using the merge sort algorithm recursively
+
+def mergeSortInterface(array):
+    mergeSort(array)
+    return array
+    
+
+def mergeSort(array):
+    pass
+
+a = [2, 3, 9, 10, 7, 8, 1, 5, 4, 6]
+
+print(mergeSortInterface(a))
