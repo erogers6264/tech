a54a1eaf66f0856bf54582c33d2f0e5e393ab281
Ethan Rogers <erogers6264@gmail.com>
2017-12-04 19:18:03 -0700

    Optimize get max profit from stock prices yesterday

--- stock_prices_yesterday.py
+++ stock_prices_yesterday.py
@@ -4,18 +4,20 @@
 # stock yesterday.
 
 
-stock_prices_yesterday = [10, 7, 5, 8, 11, 9]
+stock_prices_yesterday = [10, 7, 5, 4, 2, 1]
 
 
 def get_max_profit(stock_prices_yesterday):
-    max_profit = 0
+    max_profit = stock_prices_yesterday[1] - stock_prices_yesterday[0]
     lowest_so_far = stock_prices_yesterday[0]
     # go through every time
-    for price in stock_prices_yesterday:
-            lowest_so_far = min(lowest_so_far, price)
+    for index, price in enumerate(stock_prices_yesterday):
+            if index == 0:
+                continue
             potential_profit = price - lowest_so_far
             # New max profit is higher of the two
             max_profit = max(potential_profit, max_profit)
+            lowest_so_far = min(lowest_so_far, price)
     return max_profit
 
 print(get_max_profit(stock_prices_yesterday))
